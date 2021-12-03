630f044fa1fdd9e71207db0274e876e6c7b0b295
Yaroslav-Zozulya <91027713+Yaroslav-Zozulya@users.noreply.github.com>
2021-12-03 05:24:16 +0000

    Deploying to gh-pages from @ Yaroslav-Zozulya/project-13-ice-cream@42687b45db810e06bb1785bf1e48b34b2477f8a3 🚀

--- src.8cc9d88e.js
+++ src.8cc9d88e.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-13-ice-cream/src.7c55bd80.js.map
+//# sourceMappingURL=/project-13-ice-cream/src.8cc9d88e.js.map
