4507f34c7074eb014ec3adf3d27c98a13f9a8b46
Tkachmaxim <tkachmaxim23@gmail.com>
2021-12-06 14:47:41 +0200

    fixed_from_mentor_and_add_active_links

---
+++ src/js/active-links-mobile.js
@@ -0,0 +1,36 @@
+const getId = (link) => link.getAttribute('href').replace('#', '');
+
+const observer = new IntersectionObserver( (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting)
+         {
+            document.querySelectorAll('.menu-link').forEach((link) => {
+                link.classList.toggle(
+                    'current',
+                    getId(link) === entry.target.id
+                );
+            });
+        }
+    });
+}, {
+    threshold: 0.2,
+});
+
+
+
+
+document.querySelectorAll('section').forEach(
+    (section) => observer.observe(section),
+);
+
+
+document.querySelector('.menu-links').addEventListener('click', (Event) => {
+    if (Event.target.classList.contains('header__menu-link')) {
+        Event.preventDefault();
+
+        window.scrollTo({
+            top: document.getElementById(getId(Event.target)).offsetTop,
+            behavior: 'smooth',
+        });
+    }
+});
