adc65bd14b93e266192ef9f6d42852ada81f1581
Yaroslav Zozulya <my.dev.stories@gmail.com>
2021-12-11 23:14:34 +0300

    fixed background for section about us

--- src/js/active-link.js
+++ src/js/active-link.js
@@ -1,37 +1,29 @@
-const getId = (link) => link.getAttribute('href').replace('#', '');
+const getId = link => link.getAttribute('href').replace('#', '');
 
-const observer = new IntersectionObserver( (entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting)
-         {
-            document.querySelectorAll('.menu__link').forEach((link) => {
-                link.classList.toggle(
-                    'current',
-                    getId(link) === entry.target.id
-                );
-            });
-        }
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        document.querySelectorAll('.menu__link').forEach(link => {
+          link.classList.toggle('current', getId(link) === entry.target.id);
+        });
+      }
     });
-}, {
+  },
+  {
     threshold: 0.5,
-});
-
-
-document.querySelectorAll('section').forEach(
-    (section) => observer.observe(section),
+  },
 );
 
+document.querySelectorAll('section').forEach(section => observer.observe(section));
 
-document.querySelector('.header__menu-links').addEventListener('click', (Event) => {
-    if (Event.target.classList.contains('menu__link')) {
-        Event.preventDefault();
+document.querySelector('.header__menu-links').addEventListener('click', Event => {
+  if (Event.target.classList.contains('menu__link')) {
+    Event.preventDefault();
 
-        window.scrollTo({
-            top: document.getElementById(getId(Event.target)).offsetTop,
-            behavior: 'smooth',
-        });
-    }
+    window.scrollTo({
+      top: document.getElementById(getId(Event.target)).offsetTop,
+      behavior: 'smooth',
+    });
+  }
 });
-
-
-
